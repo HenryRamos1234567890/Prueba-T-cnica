7d255136bbfd8af69ffd000183abc4be7b26783e
HenryRamos1234567890 <henrynerio725@gmail.com>
2024-12-03 14:07:19 -0600

    primer comentario

---
+++ HenryRamos.2024.PruebaTecnica.AppWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HenryRamos._2024.PruebaTecnica.AppWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ HenryRamos.2024.PruebaTecnica.DAL/DBContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HenryRamos._2024.PruebaTecnica.DAL
+{
+    internal class DBContext
+    {
+    }
+}
